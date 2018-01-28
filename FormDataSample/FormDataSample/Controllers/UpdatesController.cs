b69853819d80a862beac1a309a02b49f7af25ace
Guangtao Zhao <zhaoguangtao@outlook.com>
2018-01-28 11:20:02 +0800

    Initial commit.

---
+++ FormDataSample/FormDataSample/Controllers/UpdatesController.cs
@@ -0,0 +1,16 @@
+﻿using FormDataSample.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace FormDataSample.Controllers
+{
+    public class UpdatesController : ApiController
+    {
+        
+    }
+}
