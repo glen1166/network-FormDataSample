a512701d3160e949dfac16c8b193fa67fba179ff
Guangtao Zhao <zhaoguangtao@outlook.com>
2018-01-28 11:27:17 +0800

    simple parameter type

--- FormDataSample/FormDataSample/Controllers/UpdatesController.cs
+++ FormDataSample/FormDataSample/Controllers/UpdatesController.cs
@@ -11,6 +11,36 @@ namespace FormDataSample.Controllers
 {
     public class UpdatesController : ApiController
     {
-        
+        static readonly Dictionary<Guid, Update> updates = new Dictionary<Guid, Update>();
+
+        [HttpPost]
+        [ActionName("Simple")]
+        public HttpResponseMessage PostSimple([FromBody] string value)
+        {
+            if (value != null)
+            {
+                Update update = new Update()
+                {
+                    Status = HttpUtility.HtmlEncode(value),
+                    Date = DateTime.UtcNow
+                };
+
+                var id = Guid.NewGuid();
+                updates[id] = update;
+
+                var response = new HttpResponseMessage(HttpStatusCode.Created)
+                {
+                    Content = new StringContent(update.Status)
+                };
+
+                response.Headers.Location =
+                    new Uri(Url.Link("DefaultApi", new { action = "status", id = id }));
+                return response;
+            }
+            else
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+        }
     }
 }
