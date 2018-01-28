83f125967cfb3fcf44f04d68cc1f65d6ecc1f762
Guangtao Zhao <zhaoguangtao@outlook.com>
2018-01-28 11:23:05 +0800

    complex parameter type

--- FormDataSample/FormDataSample/Controllers/UpdatesController.cs
+++ FormDataSample/FormDataSample/Controllers/UpdatesController.cs
@@ -11,6 +11,48 @@ namespace FormDataSample.Controllers
 {
     public class UpdatesController : ApiController
     {
-        
+        static readonly Dictionary<Guid, Update> updates = new Dictionary<Guid, Update>();
+
+        [HttpPost]
+        [ActionName("Complex")]
+        public HttpResponseMessage PostComplex(Update update)
+        {
+            if (ModelState.IsValid && update != null)
+            {
+                // Convert any HTML markup in the status text.
+                update.Status = HttpUtility.HtmlEncode(update.Status);
+
+                // Assign a new ID.
+                var id = Guid.NewGuid();
+                updates[id] = update;
+
+                // Create a 201 response.
+                var response = new HttpResponseMessage(HttpStatusCode.Created)
+                {
+                    Content = new StringContent(update.Status)
+                };
+                response.Headers.Location =
+                    new Uri(Url.Link("DefaultApi", new { action = "status", id = id }));
+                return response;
+            }
+            else
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+        }
+
+        [HttpGet]
+        public Update Status(Guid id)
+        {
+            Update update;
+            if (updates.TryGetValue(id, out update))
+            {
+                return update;
+            }
+            else
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+        }
     }
 }
